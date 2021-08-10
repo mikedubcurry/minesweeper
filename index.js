6933f1c9e90619a48be585d31b9ae10b0ecec5ff
mikedubcurry <fender18295@gmail.com>
2021-08-10 15:17:27 -0400

    is this mobile ready?

--- index.js
+++
@@ -1,5 +0,0 @@
-// missingSevens
-// given a 3x3, 2D array of integers, where every digit is between 0 and 9, except 7, find the minimum number of
-// digits to be replaced with 7s so that the sum of the numbers in each row and column are the same
-
-function missingSevens()
